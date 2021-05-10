14548dc9036af8d6dade2422cfea50b9f2096aa8
aryan basu <aryanbasu005@gmail.com>
2021-05-11 00:34:06 +0530

    login front part

--- src/meethome.js
+++ src/meethome.js
@@ -1,16 +1,26 @@
 
 import React, { Component } from 'react';
 import { Input, Button, IconButton } from '@material-ui/core';
+import firebase from "firebase";
+import Signup from './signup.js';
+import { Redirect } from 'react-router';
+
 // import GitHubIcon from '@material-ui/icons/GitHub';
 // import "./Home.css"
 
-class Meethome extends Component {
 
+class Meethome extends Component {
+	componentDidMount = () => {
+		firebase.auth().onAuthStateChanged(user => {
+		  this.setState({ isSignedIn: !!user })
+		  console.log("user", user)
+		})
+	  }
 
 	render() {
 		return (
 			<div className="container2">
-				<h1>Meet Home</h1>
+				<h1 className="text-success">Meet Home</h1>
 				<ul>
 					<li><a href="http://localhost:3000/signin">SignIn</a></li>
 					<li><a href="http://localhost:3000/signup">SignUp</a></li>
@@ -19,6 +29,17 @@ class Meethome extends Component {
 					<li><a href="http://localhost:3000/meeting">Meeting</a></li>
 					<li><a href="http://localhost:3000/meetend">MeetEnd</a></li>
 				</ul>
+      <span className="bg-light">
+				<div className="text-success">Signed In!</div>
+				<button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+				
+				<h1 className="text-success">Welcome {firebase.auth().currentUser.displayName}</h1>
+				<img
+				  alt="profile picture"
+				  src={firebase.auth().currentUser.photoURL}
+				/>
+			  </span>
+			 
 			</div>
 		)
 	}
