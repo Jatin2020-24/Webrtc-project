8b9e3d3f1534177653a6829fc408dd5a2b023c1a
Ashwani Rathee <ab669522@gmail.com>
2021-05-10 19:54:54 +0530

    Basic-Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Meetend from './meetend.js';
+import Meeting from './meeting.js';
+import Signup from './signup.js';
+import Signin from './signin.js';
+import Meethome from './meethome.js';
+import Meetpreview from './meetpreview.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Router>
+					<Switch>
+          <Route path="/" exact component={ Signup } />
+						<Route path="/signin" exact component={ Signin } />
+						<Route path="/signup" exact component={ Signup } />
+            <Route path="/meetend" exact component={ Meetend } />
+            <Route path="/meethome" exact component={ Meethome } />
+            <Route path="/meeting" exact component={ Meeting } />
+            <Route path="/meetpreview" exact component={ Meetpreview } />
+					</Switch>
+				</Router>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App ;
+// export default Initial;
---
+++ src/signup.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+import { Input, Button, IconButton } from '@material-ui/core';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+
+
+class Signup extends Component {
+
+	render() {
+		return (
+			<div className="container2">
+								<h1>SignUp</h1>
+								<ul>
+					<li><a href="http://localhost:3000/signin">SignIn</a></li>
+					<li><a href="http://localhost:3000/signup">SignUp</a></li>
+					<li><a href="http://localhost:3000/meethome">MeetHome</a></li>
+					<li><a href="http://localhost:3000/meetpreview">MeetPreview</a></li>
+					<li><a href="http://localhost:3000/meeting">Meeting</a></li>
+					<li><a href="http://localhost:3000/meetend">MeetEnd</a></li>
+				</ul>
+			</div>
+		)
+	}
+}
+
+
+export default Signup;
